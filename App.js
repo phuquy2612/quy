b3d70e56832e4f2825387d05f76315a64919f9f2
phuquy2612 <phuquy2612@gmail.com>
2024-04-09 14:06:49 +0700

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, View, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
 
 export default function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleClick = () => {
+    alert(`Tên đăng nhập: ${username}\nMật khẩu: ${password}`);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.background}> 
+        <View style={styles.content}>
+          <Image
+            style={{ width: 400, height: 400, resizeMode: 'contain', alignSelf: "center" }}
+            source={require('./assets/Dragon.png')}
+          />
+          <TextInput
+            style={[styles.textinput, styles.inputSquare]}
+            value={username}
+            onChangeText={setUsername}
+            placeholder="Tên đăng nhập"
+            placeholderTextColor="gray"
+            color="gray"
+          />
+          <TextInput
+            style={[styles.textinput, styles.inputSquare]}
+            value={password}
+            onChangeText={setPassword}
+            placeholder="Mật khẩu"
+            placeholderTextColor="gray"
+            color="gray"
+            secureTextEntry={true}
+          />
+          <TouchableOpacity style={[styles.button, styles.buttonSquare]} onPress={handleClick}>
+            <Text style={{ color: 'white' }}>Login</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +47,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  background: {
+    flex: 1,
+    backgroundColor: 'pink',
     justifyContent: 'center',
+  },
+  content: {
+    marginHorizontal: 20,
+  },
+  textinput: {
+    borderWidth: 1, 
+    borderColor: 'pink', 
+    marginVertical: 10,
+    fontSize: 20,
+    paddingHorizontal: 10,
+    backgroundColor: 'white', 
+  },
+  inputSquare: {
+    width: '100%', 
+  },
+  button: {
+    backgroundColor: 'red',
+    marginVertical: 10,
+    padding: 10,
+    alignItems: 'center',
+  },
+  buttonSquare: {
+    width: '100%',
   },
 });
